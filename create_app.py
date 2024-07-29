8043d3017c13594649bee4cc5c132b9b9d81ee9b
Paolo Gomez <peweegardner248@gmail.com>
2024-07-29 14:15:28 -0700

    update

--- create_app.py
+++ create_app.py
@@ -1,13 +1,13 @@
 # create_app.py
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://paologomez:password@localhost:5432/book_review_platform')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
